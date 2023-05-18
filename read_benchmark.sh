db0aef5cf5a7b22631e789c4c756dec0e4b8f269
Maciej Gajek <mgajek@virtuslab.com>
2023-05-18 10:35:55 +0200

    Add scripts for benchmarking

--- read_benchmark.sh
+++ read_benchmark.sh
@@ -6,15 +6,15 @@ function run_db_bench_fill() {
   shift
   shift
 
-  db_name="${filter_type}_${filter_bits}"
+  filename="${filter_type}_${filter_bits}".txt
 
   ./db_bench --benchmarks=fillrandom,overwrite,overwrite,_get_avg_size_,stats \
    --filter_type="$filter" \
     --filter_bits="$filter_bits" \
    --use_existing_db=0 \
-   --db=with_"$db_name" \
+   --db=with_"$filename" \
    --disable_compaction=0 \
-   "$@" >> "$db_name".txt
+   "$@" >> "$filename".txt
 }
 
 function run_db_bench_read() {
@@ -23,19 +23,19 @@ function run_db_bench_read() {
   shift
   shift
 
-  db_name="${filter_type}_${filter_bits}"
+  filename="${filter_type}_${filter_bits}".txt
 
-  ./db_bench --benchmarks=readrandom,readrandom,readrandom,readmissing \
+  ./db_bench --benchmarks=readrandom,readmissing,readrandom,readmissing,readrandom,readmissing \
   --filter_type="$filter" \
   --filter_bits="$filter_bits" \
   --use_existing_db=1 \
-  --db=with_"$db_name" \
+  --db=with_"$filename" \
   --disable_compaction=1 \
-  "$@" >> "$db_name".txt
+  "$@" >> "$filename".txt
 }
 
-if [ "$#" -lt 2 ]; then
-  echo "Usage: $0 filter_type filter_bits [db_bench options...]"
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 filter_type [db_bench options...]"
   exit 1
 fi
 
@@ -46,11 +46,11 @@ shift
 
 filename="${filter_type}_${filter_bits}".txt
 
-printf "\n" > $filename
-echo "#Running with filter type: $filter_type with $filter_bits#" >> $filename
+printf "\n" >> $filename
+echo "#Running Read with filter type: $filter_type with $filter_bits#" >> $filename
 printf "\n" >> $filename
 
-run_db_bench_fill "$filter_type" "$filter_bits" "$@"
+#run_db_bench_fill "$filter_type" "$filter_bits" "$@"
 run_db_bench_read "$filter_type" "$filter_bits" "$@"
 
 echo "#Benchmark done#" >> $filename
